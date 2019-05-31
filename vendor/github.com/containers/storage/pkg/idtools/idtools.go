48ef3d806689cc4d80a7c8ae3529bc99e3c1ce46
kpelzel <kpelzel@pn1706569.lanl.gov>
2019-05-31 14:33:01 -0600

    adding panic for strack trace

--- vendor/github.com/containers/storage/pkg/idtools/idtools.go
+++ vendor/github.com/containers/storage/pkg/idtools/idtools.go
@@ -284,10 +284,11 @@ func parseSubidFile(path, username string) (ranges, error) {
 
 func checkChownErr(err error, name string, uid, gid int) error {
 	if e, ok := err.(*os.PathError); ok && e.Err == syscall.EINVAL {
-		dat, readErr := ioutil.ReadFile("/proc/self/gid_map")
+		dat, readErr := ioutil.ReadFile("/proc/self/setgroups")
 		if readErr != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", dat)
 		}
+		panic("panic!")
 		return errors.Wrapf(err, "there might not be enough IDs available in the namespace (requested %d:%d for %s)", uid, gid, name)
 	}
 	return err
