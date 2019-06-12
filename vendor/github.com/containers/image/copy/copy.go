2d0f422eeb682082340707a4918dd3f3a071d57b
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-06-12 17:42:05 -0600

    debugging

--- vendor/github.com/containers/image/copy/copy.go
+++ vendor/github.com/containers/image/copy/copy.go
@@ -208,9 +208,11 @@ func Image(ctx context.Context, policyContext *signature.PolicyContext, destRef,
 		fmt.Fprintf(os.Stderr, "\nfinished copyOneImage in Image():copy.go\n\n")
 	}
 
+	fmt.Fprintf(os.Stderr, "\nStarting c.dest.Commit in Image():copy.go")
 	if err := c.dest.Commit(ctx); err != nil {
 		return nil, errors.Wrap(err, "Error committing the finished image")
 	}
+	fmt.Fprintf(os.Stderr, "\nFinished c.dest.Commit in Image():copy.go")
 
 	return manifest, nil
 }
