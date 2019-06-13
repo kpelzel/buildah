7ca1432ba192b65195001514e269637041fd4815
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-06-13 14:43:20 -0600

    adding debugging

--- vendor/github.com/containers/storage/pkg/chrootarchive/diff_unix.go
+++ vendor/github.com/containers/storage/pkg/chrootarchive/diff_unix.go
@@ -114,7 +114,12 @@ func applyLayerHandler(dest string, layer io.Reader, options *archive.TarOptions
 	cmd := reexec.Command("storage-applyLayer", dest)
 	cmd.Stdin = layer
 	fmt.Fprintf(os.Stderr, "applylayerhandler in diff_unix.go before reexec, options: %+v\n", options)
-	cmd.Env = append(cmd.Env, fmt.Sprintf("OPT=%s", data))
+	fmt.Fprintf(os.Stderr, "json data: %+v\n\n", data)
+	if cmd.Env == nil {
+		cmd.Env[0] = fmt.Sprintf("OPT=%s", data)
+	} else {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("OPT=%s", data))
+	}
 
 	outBuf, errBuf := new(bytes.Buffer), new(bytes.Buffer)
 	cmd.Stdout, cmd.Stderr = outBuf, errBuf
